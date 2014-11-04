adc6a9756f49cfe9a078f1ea5a748f3cd480a463
Bryan Leonard <b.leonard@zoho.com>
2014-11-03 20:55:15 -0500

    TOC exercise
    Seems weird but I guess you could make a useful version of it.

--- 22-contents.php
+++ 22-contents.php
@@ -8,50 +8,50 @@
 	<!-- <link rel="stylesheet" href="styles.css" type="text/css"> -->
 
 	<style>
+body {
+	background-color: #5dace7;
+}
 		
 .header {
-	height: 150 px;
-	background - color: #0174cd;
+    height: 150px;
+    background-color: #0174cd;
 }
 .header h1 {
-	width: 1000px;
-	margin: auto;
-	padding: 0;
-	line-height: 100px;
-	font-size: 40px;
-	color: # FFFFFF;
-}
-.content - frame {
-	margin: -50 px auto auto auto;
-	width: 1000 px;
-	background - color: #FFFFFF;
-	border - radius: 10 px;
-	min - height: 1300 px;
-	position: relative;
-}
-.content - frame.left {
-	margin - right: 240 px;
-	padding: 20 px;
-}
-.content - frame.left h1 {
-	margin: 0;
-}
-.content - frame.right {
-	width: 200 px;
-	padding: 10 px;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	right: 0;
-	background - color: #F1F1F1;
-	border - top - right - radius: 10 px;
-	border - bottom - right - radius: 10 px;
-}
-.content - frame.right h2 {
-	margin: 0;
-	padding: 0;
+    width: 1000px;
+    margin: auto;
+    padding: 0;
+    line-height: 100px;
+    font-size: 40px;
+    color: #FFFFFF;
 }
 
+.content-frame {
+    margin: -50px auto auto auto;
+    width: 1000px;
+    background-color: #FFFFFF;
+    min-height: 1300px;
+    position: relative;
+}
+.content-frame .left {
+    margin-right: 240px;
+    padding: 20px;
+}
+.content-frame .left h1 {
+    margin: 0;
+}
+.content-frame .right {
+    width: 200px;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: #F1F1F1;
+}
+.content-frame .right h2 {
+    margin: 0;
+    padding: 0;
+}
 	</style>
 </head>
 <body>
@@ -62,12 +62,6 @@
 		<div class="row">
 
 			<div class="col-xs-12">
-	
-	<hgroup>
-		<h1 class="page-header"></h1>
-	</hgroup>
-
-
 
 <div class="header">
     <h1>DYNAMIC TABLE OF CONTENTS</h1>
@@ -113,6 +107,25 @@
 <script>
 
 //pg 213
+var $contents = $('.content-frame').find('.left');
+var $headers = $contents.find('h1, h2, h3, h4');
+
+for (var key in $headers) {
+		var obj = $headers[key];
+}
+
+$headers.each(function(index, value){ 
+	var _header = $(value);
+	var level = parseInt(_header.context.localName. replace("h", "")); 
+
+	if (typeof _header.attr("id") != "undefined") { 
+		var listItem = $("<li><a href='#" + _header.attr("id") + "'>" + _header.html() + "</a></li>");
+	} else { 
+		var listItem = $("<li>" + _header.html() + "</li>");
+	} 
+	listItem.css("padding-left", (level * 5));
+	$('.contents').append( $(listItem))
+});
 
 
 </script>
