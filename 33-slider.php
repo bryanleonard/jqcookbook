3a77d9293013b798f3a74fcf9a68d227d25786a4
Bryan Leonard <b.leonard@zoho.com>
2014-11-11 12:04:51 -0500

    More new exercises

--- 33-slider.php
+++ 33-slider.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>tooltip plugin</title>
+	<title></title>
 
 	<!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css"> -->
 	<link rel="stylesheet" href="/assets/lib/bootstrap-3.1.1/css/bootstrap.css">
@@ -23,16 +23,9 @@
 			<div class="col-xs-12">
 	
 	<hgroup>
-		<h1 class="page-header">Tooltip plugin</h1>
+		<h1 class="page-header"></h1>
 	</hgroup>
 
-<p>&nbsp;</p>
-<p><input type="text" class="hasTooltip" data-title="This is a tooltip on an input box" /></p>
-<p>&nbsp;</p>
-<p><a href="http://www.google.com/" target="_blank"
-class="hasTooltip" title="External link to http://www.google.com/">Google.com</a></p>
-<p>&nbsp;</p>
-<button class="hasTooltip" data-title="A button with a tooltip">Button</button>
 
 
 			</div>
@@ -46,11 +39,11 @@ class="hasTooltip" title="External link to http://www.google.com/">Google.com</a
 <script src="/assets/lib/jquery-1.11.1.js"></script>
 <!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.js"></script> -->
 <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.js"></script> -->
-<script src="scripts/plugin-tooltip.js"></script>
+<!-- <script src="scripts.js"></script> -->
 
 <script>
 
-$('.hasTooltip').tooly();
+// pg 281
 
 
 
--- scripts/plugin-tooltip.js
+++ scripts/plugin-tooltip.js
@@ -1,20 +1,59 @@
 ;(function ($) {
 	
-	var name = 'pluginName';
+	var name = 'tooly';
 	Plugin.prototype = {
 		defaults: {
-
+			'height': 30,
+			'fadeInDelay': 200
 		}
 	};
 	
 	// The actual plugin constructor
 	function Plugin(element, options) {
-		var $scope = this;
-		$scope.$element = $(element);
-		$scope.element = element;
-		$scope.options = $.extend({}, this.defaults, options);
+		var _this = this;
+		_this.$el = $(element);
+		_this.el = element;
+		_this.options = $.extend({}, this.defaults, options);
 		
-		$scope.init = function () {
+		_this.init = function () {
+			_this._text = (typeof _this.$el.data('title') != "undefined") ? _this.$el.data('title') : _this.$el.prop("title");
+
+			//Only display the tooltip if a title exists.
+			if (typeof _this._text != 'undefined') {
+				var $html = $('<div class="tooltip-frame">' + '<div class="tooltip-arrow"></div>' + '<div class="tooltip-text">' + _this._text + "</div></div>");
+
+				$html.css({
+						'position': 'absolute',
+						'text-align': 'center',
+						'height': _this.options.height,
+						'line-height': _this.options.height + 'px',
+						'left': _this.$el.offset().left + _this.$el.outerWidth() - _this.$el.offset().left/1.5,
+						// 'left': _this.$el.offset().left + _this.$el.outerWidth() + 15,
+						'top': _this.$el.offset().top + (_this.$el.outerHeight() / 2) - (_this.options.height / 2),
+						'background-color': 'rgba(0, 0, 0, 0.81)',
+						'color': '#fff',
+						'padding': '0 10px',
+						'border-radius': '5px',
+						'opacity': 'none'
+					})
+				.find('.tooltip-arrow').css({
+					'width': 0,
+					'height': 0,
+					'border-top': '10px solid transparent',
+					'border-bottom': '10px solid transparent',
+					'border-right': '10px solid rgba(0, 0, 0, 0.81)',
+					'position': 'absolute',
+					'left': '-13px',
+					'top': ((_this.options.height / 2) - 10)
+				});
+
+				_this.$el.on("mouseover", function(){
+					$html.fadeIn(_this.options.fadeInDelay);
+					_this.$el.after($html);
+				}).on('mouseout', function() {
+					$html.remove();
+				});
+			}
 		}
 	}
 
